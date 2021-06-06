b71c10a7da1b47e5b9be6e27491f3594eb26eaca
amoh-godwin <amohgyebigodwin@gmail.com>
2021-06-06 17:09:22 +0000

    image from user

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
+import os
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
 from func import PhotoViewer
 
+
+if len(sys.argv) > 1:
+    user_file = 'file:///' + os.path.realpath(sys.argv[1])
 
 app = QGuiApplication(sys.argv)
 
@@ -14,5 +18,6 @@ engine.quit.connect(app.quit)
 
 back_end = PhotoViewer()
 engine.rootObjects()[0].setProperty('backend', back_end)
+engine.rootObjects()[0].setProperty('actual_image', user_file)
 
 sys.exit(app.exec())
