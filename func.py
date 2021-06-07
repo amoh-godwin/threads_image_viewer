c479a1f0aebc84e6dffdbe441a0c2299db97e02c
amoh-godwin <amohgyebigodwin@gmail.com>
2021-06-07 09:40:15 +0000

    get index

--- func.py
+++ func.py
@@ -10,6 +10,7 @@ class PhotoViewer(QObject):
     def __init__(self, currfile=""):
         super().__init__()
         self.curr_file = currfile
+        self.curr_index = 0
         self.supported_formats = ['.jpeg', '.jpg', '.png', '.gif']
         self.image_list = deque([])
 
@@ -25,4 +26,11 @@ class PhotoViewer(QObject):
             x for x in conts
              if os.path.splitext(x)[-1] in self.supported_formats])
 
+        ind = -1
+        for img in self.image_list:
+            ind += 1
+            if mainfile == img:
+                self.curr_index = ind
+
+        print(self.image_list)
 
